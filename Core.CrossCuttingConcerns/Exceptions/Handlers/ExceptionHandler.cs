8c71fb5b26078130eaca70ef405dad26e7e814a9
canyo <canyoldas5410@gmail.com>
2024-03-30 01:37:49 +0300

    Add project files.

---
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
@@ -0,0 +1,22 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Types;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Handlers;
+
+public abstract class ExceptionHandler
+{
+    public Task HandleExceptionAsync(Exception exception) =>
+        exception switch
+        {
+            AuthorizationException authorizationException => HandleException(authorizationException),
+            BusinessException businessException => HandleException(businessException),
+            NotFoundException notFoundException => HandleException(notFoundException),
+            ValidationException validationException => HandleException(validationException),
+            _ => HandleException(exception)
+        };
+
+    protected abstract Task HandleException(AuthorizationException businessException);
+    protected abstract Task HandleException(BusinessException validationException);
+    protected abstract Task HandleException(NotFoundException validationException);
+    protected abstract Task HandleException(ValidationException validationException);
+    protected abstract Task HandleException(Exception exception);
+}
---
+++ Core.CrossCuttingConcerns/Logging/Serilog/Loggers/MongoDbLogger.cs
@@ -0,0 +1,23 @@
+﻿using Core.CrossCuttingConcerns.Logging.Serilog.ConfigurationModels;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Serilog;
+
+namespace Core.CrossCuttingConcerns.Logging.Serilog.Loggers;
+
+public class MongoDbLogger : LoggerServiceBase
+{
+    public MongoDbLogger(IConfiguration configuration)
+    {
+        MongoDbConfiguration? logConfiguration = configuration
+            .GetSection("SeriLogConfigurations:MongoDbConfiguration")
+            .Get<MongoDbConfiguration>();
+
+        Logger = new LoggerConfiguration().WriteTo.MongoDBBson(configuration =>
+        {
+            MongoClient client = new(logConfiguration.ConnectionString);
+            IMongoDatabase? mongoDbInstance = client.GetDatabase(logConfiguration.Collection);
+            configuration.SetMongoDatabase(mongoDbInstance);
+        }).CreateLogger();
+    }
+}
